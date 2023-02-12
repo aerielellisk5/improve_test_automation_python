d8ebc38bff1a4a924113a47c8f8a6431e4d1c080
Aeriel Ellis <aerielellisk5@gmail.com>
2023-02-11 16:07:29 -0800

    trying to get consistent with python

---
+++ python_for_testers/variables.py
@@ -0,0 +1,16 @@
+variable_1 = 2
+variable_2 = 2
+# print(variable_1 + variable_2)
+
+variable_3 = 4
+variable_4 = 5
+# print(variable_1 * variable_4)
+
+str_1 = 'hello'
+str_2 = 'world'
+# print(str_1 + str_2)
+
+# print(1 + 'hi')
+print(str(1) + 'hi')
+
+print(type(12.5))
